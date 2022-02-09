489379edd82fdf10d2bd26b9837c349503f8bb73
Matej-Basta <71135305+Matej-Basta@users.noreply.github.com>
2022-02-09 15:44:55 +0100

    Comments for like button component

--- src/components/LikeButton.js
+++ src/components/LikeButton.js
@@ -1,14 +1,17 @@
 import React, { useState } from "react";
 
 export const LikeButton = (props) => {
+  //state that tracks the number of likes
   const [likes, setLikes] = useState(0);
 
+  //function that adds 1 like
   const addingLike = () => {
     setLikes(likes + 1);
   };
 
   return (
     <>
+      {/* creating a "likes" elements */}
       <p>{likes} Likes</p>
       <button onClick={addingLike}>Like</button>
     </>
